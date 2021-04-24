c6041a7d291aae9cb9991f851a97e5936ee5ec96
Trevrosa <49025203+Trevrosa@users.noreply.github.com>
2021-04-24 09:47:13 +0800

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,10 +1,10 @@
-git clone --no-checkout https://github.com/Trevrosa/cat-bot.git "/root/existing-dir-tmp/" 
-rm "/root/cat bot/.git/" -rf
-mv "/root/existing-dir-tmp/.git/" "/root/cat bot/"
-rm "/root/existing-dir-tmp/" -rf
-cd "/root/cat bot/"
+git clone --no-checkout https://github.com/Trevrosa/cat-bot.git "/home/trev/existing-dir-tmp/"
+rm "/home/trev/cat-bot/.git/" -rf
+mv "/home/trev/existing-dir-tmp/.git/" "/home/trev/cat-bot/"
+rm "/home/trev/existing-dir-tmp/" -rf
+cd "/home/trev/cat-bot"
 git reset --hard HEAD
 
 dotnet publish -r linux-x64
-chmod +x "/root/cat bot/bin/Debug/net5.0/linux-x64/publish/cat bot"
-"/root/cat bot/bin/Debug/net5.0/linux-x64/publish/cat bot"
+chmod +x "/home/trev/cat-bot/bin/Debug/net5.0/linux-x64/publish/cat bot"
+"/home/trev/cat-bot/bin/Debug/net5.0/linux-x64/publish/cat bot"
