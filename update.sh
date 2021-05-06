1551cfe9fadf03f1054480a1a61277a030c0902b
Trevrosa <49025203+Trevrosa@users.noreply.github.com>
2021-05-06 22:11:38 +0800

    Update update.sh

--- update.sh
+++ update.sh
@@ -7,6 +7,6 @@ mv "~/existing-dir-tmp/.git/" "~/cat-bot/"
 rm -rf "~/existing-dir-tmp/"
 git reset --hard HEAD
 
-dotnet publish -r linux-x64
+dotnet publish -r linux-x64 "~/cat-bot/cat bot.csproj"
 chmod +wxr "~/cat-bot/bin/Debug/net5.0/linux-x64/publish/cat bot"
 "~/cat-bot/bin/Debug/net5.0/linux-x64/publish/cat bot"
