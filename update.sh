09bd5eae72991b2cc4ffd447894b6bbb7c3de7d4
Trevrosa <49025203+Trevrosa@users.noreply.github.com>
2021-05-08 20:05:05 +0800

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 cd '~/cat-bot/'
-git reset --hard HEAD --
-sudo git clone --no-checkout 'https://github.com/Trevrosa/cat-bot.git' '~/existing-dir-tmp/'
+sudo git pull
 if [ -d "~/cat-bot/.git/" ]; then rm -rf "~/cat-bot/.git/" fi
 mv "~/existing-dir-tmp/.git/" "~/cat-bot/"
 rm -rf "~/existing-dir-tmp/"
