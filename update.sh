c8da0b72f281f7b9484f091ea3b4ad884b7863a9
Trevrosa <49025203+Trevrosa@users.noreply.github.com>
2021-05-08 19:56:52 +0800

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
 cd "~/cat-bot/"
-git clone --no-checkout https://github.com/Trevrosa/cat-bot.git "~/existing-dir-tmp/"
+sudo git clone --no-checkout 'https://github.com/Trevrosa/cat-bot.git' "/home/trev/existing-dir-tmp/"
+if [ -d "~/cat-bot/.git/" ]; then
 rm -rf "~/cat-bot/.git/"
+fi
 mv "~/existing-dir-tmp/.git/" "~/cat-bot/"
 rm -rf "~/existing-dir-tmp/"
 git reset --hard HEAD
 
-dotnet publish -r linux-x64 "~/cat-bot/cat bot.csproj"
+dotnet publish -r linux-x64
 chmod +wxr "~/cat-bot/bin/Debug/net5.0/linux-x64/publish/cat bot"
 "~/cat-bot/bin/Debug/net5.0/linux-x64/publish/cat bot"
