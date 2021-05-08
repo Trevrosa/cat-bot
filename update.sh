818b1ef3f69969ffe6854670b049c866845bb46c
Trevrosa <49025203+Trevrosa@users.noreply.github.com>
2021-05-08 20:05:50 +0800

    Update update.sh

--- update.sh
+++ update.sh
@@ -2,10 +2,7 @@
 
 cd '~/cat-bot/'
 sudo git pull
-if [ -d "~/cat-bot/.git/" ]; then rm -rf "~/cat-bot/.git/" fi
-mv "~/existing-dir-tmp/.git/" "~/cat-bot/"
-rm -rf "~/existing-dir-tmp/"
-git reset --hard HEAD --
+git reset --hard
 
 dotnet publish -r linux-x64
 chmod +wxr "~/cat-bot/bin/Debug/net5.0/linux-x64/publish/cat bot"
