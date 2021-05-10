43ba289d395357209bc9462f55fc8bee433993e6
Trevrosa <49025203+Trevrosa@users.noreply.github.com>
2021-05-10 23:32:31 +0800

    Update update.sh

--- update.sh
+++ update.sh
@@ -9,7 +9,8 @@ then
    sudo './bin/Debug/net5.0/linux-x64/publish/cat bot'
 else
    git reset --hard
-   dotnet publish -r linux-x64
+   dotnet restore
+   dotnet publish -r linux-x64 --no-restore
    chmod +wxr './bin/Debug/net5.0/linux-x64/publish/cat bot'
    sudo './bin/Debug/net5.0/linux-x64/publish/cat bot'
 fi
