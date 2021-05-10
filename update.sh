021e11af693c1820d1b5f1d5d97c6881b239eb3c
Trevrosa <49025203+Trevrosa@users.noreply.github.com>
2021-05-10 14:06:19 +0800

    Update update.sh

--- update.sh
+++ update.sh
@@ -3,13 +3,13 @@
 cd '/home/trev/cat-bot/'
 pull=$(sudo git pull)
 
-if [[ $pull =~ "Already up to date." ]];
+if [[ $pull =~ 'Already up to date.' ]];
 then
-   chmod +wxr "./bin/Debug/net5.0/linux-x64/publish/cat bot"
-   sudo "./bin/Debug/net5.0/linux-x64/publish/cat bot"
+   chmod +wxr './bin/Debug/net5.0/linux-x64/publish/cat bot'
+   sudo './bin/Debug/net5.0/linux-x64/publish/cat bot'
 else
    git reset --hard
    dotnet publish -r linux-x64
-   chmod +wxr "./bin/Debug/net5.0/linux-x64/publish/cat bot"
-   sudo "./bin/Debug/net5.0/linux-x64/publish/cat bot"
+   chmod +wxr './bin/Debug/net5.0/linux-x64/publish/cat bot'
+   sudo './bin/Debug/net5.0/linux-x64/publish/cat bot'
 fi
