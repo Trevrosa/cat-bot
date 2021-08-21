9760d8100894bd9753cc946a4329e3c97f66bdf1
Trevrosa <49025203+Trevrosa@users.noreply.github.com>
2021-08-21 10:10:40 +0800

    Update update.sh

--- update.sh
+++ update.sh
@@ -6,10 +6,10 @@ pull=$(sudo git pull -f)
 
 if [[ $pull =~ 'Already up to date.' ]];
 then
-   chmod +wxr './bin/Debug/net5.0/linux-x64/publish/cat bot'
+   sudo chmod +wxr './bin/Debug/net5.0/linux-x64/publish/cat bot'
    sudo './bin/Debug/net5.0/linux-x64/publish/cat bot'
 else
    sudo dotnet publish -r linux-x64
-   chmod +wxr './bin/Debug/net5.0/linux-x64/publish/cat bot'
+   sudo chmod +wxr './bin/Debug/net5.0/linux-x64/publish/cat bot'
    sudo './bin/Debug/net5.0/linux-x64/publish/cat bot'
 fi
