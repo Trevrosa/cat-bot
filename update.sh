001138572113ca1a476cea533bc0af836ccff260
Trevrosa <49025203+Trevrosa@users.noreply.github.com>
2021-08-21 10:08:00 +0800

    Update update.sh

--- update.sh
+++ update.sh
@@ -9,9 +9,7 @@ then
    chmod +wxr './bin/Debug/net5.0/linux-x64/publish/cat bot'
    sudo './bin/Debug/net5.0/linux-x64/publish/cat bot'
 else
-   git reset --hard
-   sudo dotnet restore
-   sudo dotnet publish -r linux-x64 --no-restore
+   sudo dotnet publish -r linux-x64
    chmod +wxr './bin/Debug/net5.0/linux-x64/publish/cat bot'
    sudo './bin/Debug/net5.0/linux-x64/publish/cat bot'
 fi
