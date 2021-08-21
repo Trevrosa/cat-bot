f3646d2305564814b0121fdc0e2dc7533e5248a7
Trevrosa <49025203+Trevrosa@users.noreply.github.com>
2021-08-21 10:07:27 +0800

    Update update.sh

--- update.sh
+++ update.sh
@@ -6,12 +6,12 @@ pull=$(sudo git pull -f)
 
 if [[ $pull =~ 'Already up to date.' ]];
 then
-   chmod +wxr './bin/Debug/net*/linux-x64/publish/cat bot'
-   sudo './bin/Debug/net*/linux-x64/publish/cat bot'
+   chmod +wxr './bin/Debug/net5.0/linux-x64/publish/cat bot'
+   sudo './bin/Debug/net5.0/linux-x64/publish/cat bot'
 else
    git reset --hard
    sudo dotnet restore
    sudo dotnet publish -r linux-x64 --no-restore
-   chmod +wxr './bin/Debug/net*/linux-x64/publish/cat bot'
-   sudo './bin/Debug/net*/linux-x64/publish/cat bot'
+   chmod +wxr './bin/Debug/net5.0/linux-x64/publish/cat bot'
+   sudo './bin/Debug/net5.0/linux-x64/publish/cat bot'
 fi
